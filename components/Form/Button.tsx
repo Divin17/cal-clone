3c28347a0f285fad984e813feb5729c9ce6d0400
ISHIMWE Divin <divinfiston1@gmail.com>
2022-02-20 21:10:40 +0200

    Form Button

---
+++ components/Form/Button.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Spinner from "./Spinner";
+
+export type Props = {
+   buttonText: string;
+   isDisabled: boolean;
+   onClick?: (event: React.FormEvent<HTMLButtonElement>) => void;
+   isLoading: boolean;
+};
+
+const Button: React.FC<Props> = (props) => {
+   return (
+      <button
+         className={`${
+            props.isDisabled
+               ? "ring-1 text-red-300 ring-red-300 cursor-not-allowed"
+               : "bg-green-500 hover:bg-green-400"
+         } w-full text-white font-bold py-2 px-4 rounded mt-4 content-center`}
+         type="submit"
+         onClick={props.onClick}
+         disabled={props.isDisabled}
+      >
+         {props.isLoading ? (
+            <Spinner class="w-6 h-6 border-[2px] border-white border-dotted rounded-full" />
+         ) : (
+            props.buttonText
+         )}
+      </button>
+   );
+};
+export default Button;
